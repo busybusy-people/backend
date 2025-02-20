505da16e51afe998ce6a774524245fdcd66cc111
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-02-20 18:16:01 +0900

    초기세팅

---
+++ src/main/java/com/example/busybusy/BusybusyApplication.java
@@ -0,0 +1,13 @@
+package com.example.busybusy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusybusyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusybusyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/busybusy/BusybusyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busybusy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusybusyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
